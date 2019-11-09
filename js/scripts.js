1aea9e7427590aa470893e89435f14cc773e81ff
Saul Ordonez <sauleordonez@gmail.com>
2019-11-08 16:40:10 -0800

    add else statement to prevent user error

--- js/scripts.js
+++ js/scripts.js
@@ -12,32 +12,41 @@ Pizza.prototype.pizzaCost = function() {
   if (this.size === '10"') {
     this.cost += 10;
   }
-  if (this.size === '14"') {
+  else if (this.size === '14"') {
     this.cost += 12;
   }
-  if (this.size === '18"') {
+  else if (this.size === '18"') {
     this.cost += 14;
   }
-  if (this.size === '24"') {
+  else if (this.size === '24"') {
     this.cost += 16;
+  }
+  else {
+    alert('Please select a size');
   }
   if (this.crust === 'Original') {
     this.cost += 0;
   }
-  if (this.crust === 'Thin Crust') {
+  else if (this.crust === 'Thin Crust') {
     this.cost += 2;
   }
-  if (this.crust === 'Deep Dish') {
+  else if (this.crust === 'Deep Dish') {
     this.cost += 2;
   }
-  if (this.crust === 'New York') {
+  else if (this.crust === 'New York') {
     this.cost += 2;
+  }
+  else {
+    alert('Please select a crust type');
   }
   for (i = 0; i < this.toppings.length; i ++) {
       this.cost += 1;
   }
   for (i = 0; i < this.dippingSauces.length; i ++) {
       this.cost += 1;
+  }
+  if (this.cost === 0) {
+    alert('Please enter information to complete form');
   }
   // if ('#breadstickButton' === 1) {
   //   this.cost += 6;
@@ -64,14 +73,18 @@ $(document).ready(function() {
     var newOrder = new Pizza(size, crust, toppings, dippingSauces);
     newOrder.pizzaCost();
     $('#pizzaPrice').html(newOrder.cost);
-    $('.totalPrice').show();
+    if (newOrder.cost > 0) {
+      $('.totalPrice').show();
+    }
 
     $('#showSize').text(size);
     $('#showCrust').text(crust);
     $('#showToppings').text(newOrder.toppings);
     $('#showDippingsauces').text(newOrder.dippingSauces);
     // $('#showBreadsticks').text(newOrder.breadsticks);
-    $('#yourOrder').show();
+    if (newOrder.cost > 0) {
+      $('#yourOrder').show();
+    }
   });
 
   // $('#breadstickButton').click(function(event) {
